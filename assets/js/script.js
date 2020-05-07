d6d69bbff120ca9d4ec61d8a83e3ed3d14b119d6
J Maine <33839097+xjmaine@users.noreply.github.com>
2020-05-07 14:16:01 +0000

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+var jprice = [
+    {
+        "Package": "Bronze",
+        "Price": "$150",
+    },
+    {
+        "Package": "Silver",
+        "Price": "$150",
+    },
+    {
+        "Package": "Gold",
+        "Price": "$350",
+    }
+]
+
+var output = document.getElementById('output');
+output.innerHTML = jprice.Price;
